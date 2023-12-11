b57db25a7fddebad6b6861750e0d46c385ecd363
zlhshn <zsahin4401@gmail.com>
2023-12-12 01:11:17 +0300

    start

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import { MdFavorite } from "react-icons/md";
 import { useState } from "react";
+import { IoStarSharp } from "react-icons/io5";
 
 const ProductCard = ({title,price ,image ,id}) => {
 
@@ -10,7 +11,7 @@ const ProductCard = ({title,price ,image ,id}) => {
   return (
     <Card key={id} className="rounded-2 m-auto card" role="button">
       <Card.Header className="d-flex justify-content-between">
-        <Card.Title>{price} $</Card.Title>
+        <Card.Title className="d-flex align-items-center gap-2"><IoStarSharp className="text-warning" /> {price} </Card.Title>
         <MdFavorite size={30} className={show ? "text-danger" : "text-dark"} onClick={()=>setShow(!show)}/>
       </Card.Header>
       <Card.Img variant="top" src={image} className="player-logo" />
